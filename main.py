084445c238c7248e5cd07a6000e1c5da0914b3bd
ingobeans <127751491+ingobeans@users.noreply.github.com>
2024-12-02 19:47:13 +0100

    performance fixes

--- main.py
+++ main.py
@@ -1,42 +1,14 @@
-import sources, os, base64, sys, shutil, pygame, pygame_gui
-
-games = []
-
-def game_to_dict(game):
-    return {"name":game.name, "source":game.parent_source.name, "id":game.id, "illustration": game.illustration_path != None}
-
-def get_illustration_data(game_id):
-    game = [g for g in games if g.id == str(game_id)][0]
-    path = game.illustration_path
-    if os.path.isfile(path):
-        with open(path, "rb") as f:
-            data = f.read()
-    else:
-        return None
-    
-    base64_data = base64.b64encode(data).decode('utf-8')
-    return f"data:image/jpeg;base64,{base64_data}"
-
-def run_game(id):
-    print(id)
-    game = [g for g in games if g.id == str(id)][0]
-    game.parent_source.run_game(id)
-
-def get_games():
-    global games
-    games = sources.get_games()
-    return [game_to_dict(g) for g in games]
+import sources, pygame, pygame_gui, datetime
 
 class Window:
     def __init__(self):
         pygame.init()
-        self.screen = pygame.display.set_mode([1196, 675],pygame.RESIZABLE)#|pygame.NOFRAME)
+        self.screen = pygame.display.set_mode([1196, 675],vsync=True,flags=pygame.RESIZABLE)#|pygame.NOFRAME)
         self.running = False
         self.background_color = (25,25,27)
         self.card_color = (61,61,67)
         self.ui_manager = pygame_gui.UIManager([1196, 675])
         pygame.display.set_caption("Basic Launcher")
-        self.clock = pygame.time.Clock()
         self.scrollbox = pygame_gui.elements.UIScrollingContainer(self.screen.get_rect(),self.ui_manager)
         self.buttons = []
         self.corners_image = pygame.image.load("assets/corners.png")
@@ -48,9 +20,14 @@ class Window:
         self.card_gap_y = 10
 
     def run(self):
+        self.last_frame = datetime.datetime.now()
         self.running = True
         while self.running:
-            time_delta = self.clock.tick(60) / 1000
+            now = datetime.datetime.now()
+            time_delta = (now - self.last_frame).total_seconds()
+            self.last_frame = now
+            print(f"FPS: {round(1/time_delta)}")
+
             for event in pygame.event.get():
                 if event.type == pygame.QUIT:
                     self.running = False
@@ -61,7 +38,7 @@ class Window:
                     for button in self.buttons:
                         if button[0] == event.ui_element:
                             print(button[1])
-                            button[1].parent_source.run_game(button[1].id)
+                            button[1].parent_source.run_game(button[1].id)          
                 self.ui_manager.process_events(event)
             
             self.ui_manager.update(time_delta)
@@ -73,15 +50,7 @@ class Window:
         pygame.quit()
 
     def draw_buttons(self):
-        for button,game,artwork in self.buttons:
-            card = pygame.Surface((button.rect[2],button.rect[3]))
-            card.fill(self.card_color)
-            if artwork:
-                card.blit(artwork)
-            
-            text = self.font.render(game.name,True,(255,255,255))
-            card.blit(text,((self.card_width - text.width) / 2,225+5))
-            card.blit(self.corners_image)
+        for button,_,card in self.buttons:
             self.screen.blit(card,button.rect)
 
     def calc_button_pos(self, index):
@@ -99,11 +68,21 @@ class Window:
         return button
 
     def add_game_button(self, game:sources.game.Game):
+        button = self.create_game_button(game)
+
+        card = pygame.Surface((button.rect[2],button.rect[3]))
+        card.fill(self.card_color)
+        
+        text = self.font.render(game.name,True,(255,255,255))
+        card.blit(text,((self.card_width - text.width) / 2,225+5))
+        card.blit(self.corners_image)
         artwork = None
         if game.illustration_path:
             artwork = pygame.image.load(game.illustration_path)
             artwork = pygame.transform.scale(artwork,(self.card_width, 225))
-        self.buttons.append([self.create_game_button(game),game,artwork])
+            card.blit(artwork)
+        
+        self.buttons.append([button,game,card])
 
 window = Window()
 for game in sources.get_games():
